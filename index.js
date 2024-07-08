fd834a8ea52438c7579fdbb6a69af84a9c2bc77c
Salvatore Aresco <s.aresco@agilesrl.com>
2024-07-08 14:36:56 +0200

    Fixes

--- index.js
+++ index.js
@@ -6,25 +6,41 @@ const port = 3000
 app.get('/WSSDORICOVERI/:params', (req, res) => {
     const params = req.params.params;
 
-    // params is like /WSSDORICOVERI/{$codicePrenotazione}%7C{$tipoEvento}{$idSDO}
-    // params is: pren1|Psdo1
+    // WS path accepts: /WSSDORICOVERI/{$codicePrenotazione}%7C{$tipoEvento}{$idSDO}
+    // this params contains {codicePrenotazione}|{tipoEvento}{idSDO}
+
     const splitted = params.split('|');
+
     const codicePrenotazione = splitted[0];
+    if (!codicePrenotazione) return res.status(400).json({
+        error: "codicePrenotazione is required"
+    });
+
     const tipoEvento = splitted[1][0];
-    const idSDO = params.substring(splitted[0].length + tipoEvento.length + 1, params.length - 1);
+    if (!tipoEvento) throw res.status(400).send("tipoEvento is required");
+    if(!["P", "R", "V"].includes(tipoEvento)) return res.status(400).json({
+        error: "tipoEvento must be P, R or V"
+    })
+
+    // regex if theres something after tipoEvento
+    const idSDO = splitted[1].replace(/[A-Za-z]/g, '');
+    if (!idSDO) return res.status(400).json({
+        error: "idSDO is required"
+    });
 
     console.log("\n[NUOVA RICHIESTA]");
     console.log(`codicePrenotazione: ${codicePrenotazione}`);
     console.log(`tipoEvento: ${tipoEvento}`);
     console.log(`idSDO: ${idSDO}`);
 
-    res.send(`
-        <p>codicePrenotazione: ${codicePrenotazione}</p>
-        <p>tipoEvento: ${tipoEvento}</p>
-        <p>idSDO: ${idSDO}</p>    
-    `)
+    const res_json = {
+        esito: "OK",
+        operazione: "WSSDORICOVERI",
+    }
+
+    res.status(200).send(res_json);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`WebSanity Simulator listening on port ${port}`)
 })
